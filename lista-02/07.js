47545581d49b93a662d1047f483f5063aedfe5ba
Rafiki <rafikidias.cy@gmail.com>
2024-03-06 21:08:56 -0300

    update

---
+++ lista-02/07.js
@@ -0,0 +1,7 @@
+// Crie um algoritmo que calcule e mostre o novo valor de um salário a partir das seguintes regras: 
+// salários de até R$ 1.000,00 inclusive recebem 30% de aumento; 
+// salários de até R$ 2.000,00 inclusive recebem 25%;
+// salários de até R$ 3.000,00 inclusive recebem 20%;
+// salários de até R$ 4.000,00 inclusive recebem 15% 
+// e salários acima de R$ 4.000,00 recebem 10%.
+
