7071dc1bf409c236d886c9f1314438953fd66f07
Rafiki <rafikidias.cy@gmail.com>
2024-03-07 23:26:13 -0300

    Update 07.js

--- lista-02/07.js
+++ lista-02/07.js
@@ -5,3 +5,20 @@
 // salários de até R$ 4.000,00 inclusive recebem 15% 
 // e salários acima de R$ 4.000,00 recebem 10%.
 
+let salarioAtual = 2500;
+let novoSalario;
+
+if (salarioAtual <= 1000) {
+    novoSalario = salarioAtual * 1.3;
+} else if (salarioAtual <= 2000) {
+    novoSalario = salarioAtual * 1.25;
+} else if (salarioAtual <= 3000) {
+    novoSalario = salarioAtual * 1.2;
+} else if (salarioAtual <= 4000) {
+    novoSalario = salarioAtual * 1.15;
+} else {
+    novoSalario = salarioAtual * 1.1;
+}
+
+console.log("Salário atual: R$" + salarioAtual.toFixed(2));
+console.log("Novo salário: R$" + novoSalario.toFixed(2));
